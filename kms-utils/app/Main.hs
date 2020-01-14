b2964c2e0b55ece83f0211473fa0dbd7102af774
Mo Mirza <mohd.uraib@gmail.com>
2020-01-14 13:57:20 +0000

    Add encryption support

--- kms-utils/app/Main.hs
+++ kms-utils/app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
 import Lib
+import Data.Text
 
 main :: IO ()
-main = someFunc
+main = do 
+    mct <- encryptPlainText "alias/prod" "foo"
+    case mct of 
+        Just ct -> print ct
+        Nothing -> putStrLn  "Unable to encrypt"
--- kms-utils/src/Lib.hs
+++ kms-utils/src/Lib.hs
@@ -1,12 +1,42 @@
 module Lib
-    ( encrypt
-    ) where
+    ( encryptPlainText
+    , decryptCipherText
+    )
+where
 
-data PlainText = PlainText String
+import           Control.Lens
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.AWS
+import           Data.ByteString         hiding ( pack )
+import           Data.ByteString.Base64
+import           Data.ByteString.Builder        ( hPutBuilder )
+import           Data.Conduit
+import qualified Data.Conduit.List             as CL
+import           Data.Monoid
+import           Data.Text
+import           Data.Word
+import           Network.AWS.Data
+import           Network.AWS.KMS
+import           System.IO
 
-data CipherText = CipherText String
 
+type PlainText = String
+type KeyAlias = String
+type RawCipherText = ByteString
+type CipherText = ByteString
 
-encrypt :: PlainText ->  IO CipherText
-encrypt = undefined 
+enc :: PlainText -> KeyAlias -> IO (Maybe RawCipherText)
+enc pt ka = do
+    env <- newEnv Discover
+    let request = encrypt (pack ka) (toBS pt)
+    runResourceT $ runAWST env $ within Ireland $ do
+        resp <- send request
+        return $ view ersCiphertextBlob resp
 
+encryptPlainText :: PlainText -> KeyAlias -> IO (Maybe CipherText)
+encryptPlainText pt ka = do
+    ct <- enc pt ka
+    return $ fmap encode ct
+
+decryptCipherText :: CipherText -> IO PlainText
+decryptCipherText = undefined
